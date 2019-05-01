461354d5a9a15cf2a790d4af532c5a4e1a146af5
Kyle Barnett <kbarnett23@gmail.com>
2019-04-30 20:24:32 -0600

    advanced for hosting

---
+++ JS/EventListeners.js
@@ -0,0 +1,6 @@
+// let hoverButton = document.querySelectorAll('.demo-pics').forEach(pic => {
+//   pic.addEventListener('click', event => {
+//     console.log('you clicked me ')
+//   })
+// })
+let 
